a04886df22a044f7827dfd5b4117a51030a4f1f8
ZephyRemix <hunyaochong@gmail.com>
2024-12-08 13:39:35 +0800

    update before styling

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import getLocationByIp from './geolocation';
 import loadWeather from './weather';
 
 import './styles/weather_hero.css';
+import './styles/config.css';
 
 async function initWeather() {
   const data = await getLocationByIp();
